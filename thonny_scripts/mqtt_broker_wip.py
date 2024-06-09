1b9a9557d88afb9864cfc2a95da6e9ac8da42e6b
Allan Nørrum Lassesen <201710818@post.au.dk>
2024-06-09 20:55:20 +0200

    adding debug LED

--- thonny_scripts/mqtt_broker_wip.py
+++ thonny_scripts/mqtt_broker_wip.py
@@ -7,13 +7,19 @@ import time
 import gc
 import threading
 import random
+import machine, neopixel
 
 class MQTTBroker:
-    def __init__(self, host='0.0.0.0', port=1883):
+    def __init__(self, host='0.0.0.0', port=1883, rgb_led = -1, debug = False):
         self.host = host
         self.port = port
         self.clients = {}
         self.topics = {}
+        self.rgb_led = rgb_led
+        self.debug = debug
+        if rgb_led > 0:
+            self.led = neopixel.NeoPixel(machine.Pin(rgb_led), 1)
+            self.set_led((0,255,0))
 
     def add_client(self, client_socket):
         # Add a new client to the list of connected clients
@@ -24,32 +30,21 @@ class MQTTBroker:
         del self.clients[client_socket]
 
     def start(self):
-        # Connect to WiFi (replace with your SSID and password)
-        ssid = 'Signes_hytte_2.4GHz'
-        password = '2744igen'
-                       
-        wifi = network.WLAN(network.STA_IF); wifi.active(True)
-        wifi.scan()                             # Scan for available access points
-        wifi.connect("Signes_hytte_2.4GHz", "2744igen") # Connect to an AP
-        wifi.isconnected()                      # Check for successful connection
-        while not wifi.isconnected():
-            time.sleep(1)
-        
         addr = socket.getaddrinfo(self.host, self.port)[0][-1]
         s = socket.socket()
         s.bind(addr)
         s.listen(5)
-        print("MQTT broker listening on {}:{}".format(wifi.ifconfig()[0], self.port))
+        self.printI(f"Info: MQTT broker listening on {self.host}:{self.port}")
 
         while True:
             try:
                 cl, addr = s.accept()
-                print('Client connected from', addr)
+                self.printI(f'Info: Client connected from {addr}')
                 cl.settimeout(60.0)
                 self.add_client(cl)
                 threading.Thread(target=self.handle_client, args=(cl,)).start()
             except Exception as e:
-                print('Error:', e)
+                self.printE(f'Error: {e}')
                 self.cleanup_clients()
 
     def handle_client(self, client):
@@ -59,7 +54,7 @@ class MQTTBroker:
             while True:
                 msg = client.recv(2048)
                 if not msg:
-                    print("No message received, closing connection.")
+                    self.printE("Error: No message received, closing connection.")
                     self.remove_client(client)
                     break
                 
@@ -82,13 +77,14 @@ class MQTTBroker:
                     msg = buffer[:total_length]
                     buffer = buffer[total_length:]
                 
-                print('Received message:', msg, ' - ', ((msg[0] & 0xF0) >> 4) )
+                self.printD(f'Debug: Received message:{msg} - {((msg[0] & 0xF0) >> 4)}' )
                 msg_type = (msg[0] & 0xF0) >> 4
-                print('Message type:', msg_type)
+                self.printD(f'Debug: Message type: {msg_type}')
                 if msg_type == 1:  # CONNECT
                     self.handle_connect(client, msg)
                 elif msg_type == 3:  # PUBLISH
                     self.handle_publish(client, msg)
+                    self.set_led(( 0, 0, 255))
                 elif msg_type == 8:  # SUBSCRIBE
                     self.handle_subscribe(client, msg)
                 elif msg_type == 10:  # UNSUBSCRIBE
@@ -98,12 +94,14 @@ class MQTTBroker:
                 elif msg_type == 14:  # DISCONNECT
                     self.handle_disconnect(client)
                 else:
-                    print('Unknown message type:', msg_type)
+                    self.printW(f'Warning: Unknown message type: {msg_type}')
+                self.set_led(( 0, 255, 0))
         except Exception as e:
-            print('Client handling error:', e)
+            self.printE(f'Error: Client handling error: {e}')
             self.remove_client(client)
+            self.set_led(( 255, 0, 0))
         finally:
-            print('Closing client connection.')
+            self.printW('Warning: Closing client connection.')
             client.close()
 
     def handle_connect(self, client, msg):
@@ -111,25 +109,25 @@ class MQTTBroker:
             try:
                 protocol_name_len = struct.unpack('>H', msg[2:4])[0]
                 protocol_name = msg[4:4 + protocol_name_len].decode('utf-8')
-                print('Protocol Name:', protocol_name)
+                self.printD(f'Debug: Protocol Name: {protocol_name}')
                 
                 # Check if the protocol name is MQTT
                 if protocol_name != 'MQTT':
-                    print('Unsupported protocol:', protocol_name)
+                    self.printE(f'Error: Unsupported protocol: {protocol_name}')
                     raise ValueError('Unsupported protocol')
                 
                 client_id_len = struct.unpack('>H', msg[10 + protocol_name_len + 1:12 + protocol_name_len + 1])[0]
                 client_id = msg[12 + protocol_name_len + 1:12 + protocol_name_len + 1 + client_id_len].decode('utf-8')
                 self.clients[client_id] = client
                 client.send(b'\x20\x02\x00\x00')  # CONNACK with 0x00 connection accepted
-                print('Client connected:', client_id)
+                self.printD(f'Debug: Client connected: {client_id}')
                 
             except ValueError as ve:
-                print('Error in handle_connect:', ve)
+                self.printE(f'Error: Error in handle_connect: {ve}')
                 # Close the client connection
                 client.close()
             except Exception as e:
-                print('Error in handle_connect:', e)
+                self.printE(f'Error: Error in handle_connect: {e}')
                 # Close the client connection
                 client.close()
         else:
@@ -145,7 +143,7 @@ class MQTTBroker:
             payload = msg[4 + topic_len:]
 
             # Log the received message and topic
-            print('Received message:', payload, 'on topic:', topic)
+            self.printR(f'Recieve: Received message: {payload} on topic: {topic}')
 
             # Forward the message to subscribers
             if topic in self.topics:
@@ -154,24 +152,24 @@ class MQTTBroker:
                         if True:# self.is_socket_open(subscriber):
                             subscriber.send(msg)  # Forward the entire message
                         else:
-                            print('Subscriber socket closed, removing from topic:', subscriber)
+                            self.printW(f'Warning: Subscriber socket closed, removing from topic: {subscriber}')
                             self.topics[topic].remove(subscriber)
                     except OSError as e:
-                        print('Error forwarding message to subscriber:', e)
-                        print('Removing subscriber:', subscriber)
+                        self.printE(f'Error: Error forwarding message to subscriber: {e}')
+                        self.printE(f'Error: Removing subscriber: {subscriber}')
                         # self.topics[topic].remove(subscriber)
             else:
-                print('No subscribers for topic:', topic)
+                self.printW(f'Warning: No subscribers for topic: {topic}')
 
-            print('Message published to topic:', topic)
+            self.printS(f'Publish: {payload} published to topic: {topic}')
         except Exception as e:
-            print('Error in handle_publish:', e)
+            self.printE(f'Error: Error in handle_publish: {e}')
 
 
 
     def handle_subscribe(self, client, msg):
         try:
-            print("Received subscription message:", msg)
+            self.printD(f"Debug: Received subscription message: {msg}")
             topics = []
             packet_id = msg[2:4]
             index = 4
@@ -187,7 +185,7 @@ class MQTTBroker:
                 topic_len = struct.unpack('>H', msg[index:index+2])[0]
                 index += 2
 
-                print(f"Read topic length: {topic_len} at index {index - 2}")
+                self.printD(f"Debug: Read topic length: {topic_len} at index {index - 2}")
 
                 # Ensure there are enough bytes to read the topic
                 if index + topic_len > len(msg):
@@ -197,7 +195,7 @@ class MQTTBroker:
                 topic = msg[index:index + topic_len].decode('utf-8')
                 index += topic_len
 
-                print(f"Read topic: {topic} at index {index - topic_len}")
+                self.printD(f"Debug: Read topic: {topic} at index {index - topic_len}")
 
                 # Ensure there is a QoS byte
                 if index >= len(msg):
@@ -207,7 +205,7 @@ class MQTTBroker:
                 qos = msg[index]
                 index += 1
 
-                print(f"Read QoS: {qos} at index {index - 1}")
+                self.printD(f"Debug: Read QoS: {qos} at index {index - 1}")
 
                 if topic in self.topics:
                     self.topics[topic].append(client)
@@ -219,11 +217,11 @@ class MQTTBroker:
 
             # Respond with a SUBACK for the first topic's packet_id (assuming single subscription)
             client.send(b'\x90\x03' + packet_id + b'\x00')  # SUBACK
-            print('Client subscribed to topics:', topics)
+            self.printR(f'Subscription: Client subscribed to topics: {topics}')
 
         except Exception as e:
             # Send SUBNACK message
-            print('Error in handle_subscribe, Sent SUBNACK due to error:', e)
+            self.printE(f'Error: Error in handle_subscribe, Sent SUBNACK due to error: {e}')
             client.send(b'\x90\x03' + packet_id + b'\x80')
 
 
@@ -237,9 +235,9 @@ class MQTTBroker:
                     del self.topics[topic]
             packet_id = msg[2:4]
             client.send(b'\xb0\x02' + packet_id)  # UNSUBACK
-            print('Client unsubscribed from topic:', topic)
+            self.printI(f'Info: Client unsubscribed from topic: {topic}')
         except Exception as e:
-            print('Error in handle_unsubscribe:', e)
+            self.printE(f'Error: Error in handle_unsubscribe: {e}')
             
     def handle_disconnect(self, client):
         client_id = None
@@ -250,11 +248,11 @@ class MQTTBroker:
         if client_id:
             del self.clients[client_id]
         client.close()
-        print('Client disconnected:', client_id)
+        self.printW(f'Warning: Client disconnected: {client_id}')
 
     def send_pingresp(self, client):
         client.send(b'\xd0\x00')  # PINGRESP
-        print('Sent PINGRESP')
+        self.printD('Debug: Sent PINGRESP')
 
     def cleanup_clients(self):
         for client in self.clients.values():
@@ -272,6 +270,50 @@ class MQTTBroker:
             return True
         except OSError:
             return False
+    
+    def printD(self, text):
+        if self.debug:
+            print(text)
+    
+    def printI(self, text):
+        print(self.INFO + self.BOLD + text + self.CLEAR)
+    
+    def printW(self, text):
+        print(self.WARNING + self.BOLD + text + self.CLEAR)
+    
+    def printE(self, text):
+        print(self.ERROR + self.BOLD + text + self.CLEAR)
+        
+    def printS(self, text):
+        print(self.SEND + self.BOLD + text + self.CLEAR)
+    
+    def printR(self, text):
+        print(self.RECIEVE + self.BOLD + text + self.CLEAR)
+    
+    def set_led(self, color):
+        self.led[0] = color
+        self.led.write()
+    
+    HEADER = '\033[95m'
+    SEND = '\033[94m'
+    RECIEVE = '\033[96m'
+    INFO = '\033[92m'
+    WARNING = '\033[93m'
+    ERROR = '\033[91m'
+    CLEAR = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
-broker = MQTTBroker()
+# Connect to WiFi (replace with your SSID and password)
+ssid = 'Signes_hytte_2.4GHz'
+password = '2744igen'
+               
+wifi = network.WLAN(network.STA_IF); wifi.active(True)
+wifi.scan()                             # Scan for available access points
+wifi.connect("Signes_hytte_2.4GHz", "2744igen") # Connect to an AP
+wifi.isconnected()                      # Check for successful connection
+while not wifi.isconnected():
+    time.sleep(1)
+
+broker = MQTTBroker(wifi.ifconfig()[0], 1883, 48, True)
 broker.start()
