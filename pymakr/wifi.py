2ac294e60c9b4f33d55ca014e581dfe11aa09727
Jesper Veggerby <jesper@veggerby.com>
2024-10-30 18:13:05 +0100

    scan networks

--- pymakr/wifi.py
+++ pymakr/wifi.py
@@ -1,52 +1,60 @@
-import time
-import network
+    import time
+    import network
 
-# Configuration for the Access Point
-SSID = 'frivillig'
-PASSWORD = 'test1234'
+    # Configuration for the Access Point
+    SSID = 'frivillig'
+    PASSWORD = 'test1234'
 
-sta_if = network.WLAN(network.STA_IF)
+    sta_if = network.WLAN(network.STA_IF)
 
-def connect_to_wifi(ssid=SSID, password=PASSWORD, retries=5):
-    #ap_if = network.WLAN(network.AP_IF)
-    sta_if.active(True)
-    sta_if.scan()
-    sta_if.connect(ssid, password)
+    def scan_wifi():
+        sta_if.active(True)
+        list = sta_if.scan()
+        print('Available networks:')
+        for ssid in list:
+            print(ssid[0].decode())
+        sta_if.active(False)
 
-    print(f'Connecting to WiFi {ssid}', end="")
-    retry = retries
-    while not sta_if.isconnected() and retry > 0:
-        time.sleep(1)
-        print('.', end="")
-        retry -= 1
+    def connect_to_wifi(ssid=SSID, password=PASSWORD, retries=5):
+        #ap_if = network.WLAN(network.AP_IF)
+        sta_if.active(True)
+        sta_if.scan()
+        sta_if.connect(ssid, password)
 
-    return sta_if.isconnected()
-
-def network_config():
-    print('Network config:', sta_if.ifconfig())
-
-def try_connect_to_wifi(ssid=SSID, password=PASSWORD):
-    retry = 10
-    if sta_if.isconnected():
-        print('Already connected')
-    else:
-        while not connect_to_wifi(ssid, password) and retry > 0:
-            disable()
+        print(f'Connecting to WiFi {ssid}', end="")
+        retry = retries
+        while not sta_if.isconnected() and retry > 0:
             time.sleep(1)
+            print('.', end="")
             retry -= 1
 
-    network_config()
+        return sta_if.isconnected()
 
-def disable():
-    sta_if.active(False)
-    print('disconnected')
+    def network_config():
+        print('Network config:', sta_if.ifconfig())
 
-def start_hotspot(ssid=SSID, password=PASSWORD):
-    # Create an access point (AP) interface
-    ap = network.WLAN(network.AP_IF)
-    ap.config(essid=ssid, password=password)
-    ap.active(True)
-    print(f"Access Point {ssid} started with IP: {ap.ifconfig()[0]}")
+    def try_connect_to_wifi(ssid=SSID, password=PASSWORD):
+        retry = 10
+        if sta_if.isconnected():
+            print('Already connected')
+        else:
+            while not connect_to_wifi(ssid, password) and retry > 0:
+                disable()
+                time.sleep(1)
+                retry -= 1
+
+        network_config()
+
+    def disable():
+        sta_if.active(False)
+        print('disconnected')
+
+    def start_hotspot(ssid=SSID, password=PASSWORD):
+        # Create an access point (AP) interface
+        ap = network.WLAN(network.AP_IF)
+        ap.config(essid=ssid, password=password)
+        ap.active(True)
+        print(f"Access Point {ssid} started with IP: {ap.ifconfig()[0]}")
 
 
 
