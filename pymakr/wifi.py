d6e09f141e7163aed3c2496123b99d98c31acf27
Jesper Veggerby <jesper@veggerby.com>
2024-10-30 17:50:36 +0100

    retry connect to wifi

--- pymakr/wifi.py
+++ pymakr/wifi.py
@@ -7,31 +7,36 @@ PASSWORD = 'test1234'
 
 sta_if = network.WLAN(network.STA_IF)
 
-def connect_to_wifi(ssid=SSID, password=PASSWORD):
+def connect_to_wifi(ssid=SSID, password=PASSWORD, retries=5):
     #ap_if = network.WLAN(network.AP_IF)
     sta_if.active(True)
     sta_if.connect(ssid, password)
 
     print(f'Connecting to WiFi {ssid}', end="")
-    while not sta_if.isconnected():
+    retry = retries
+    while not sta_if.isconnected() and retry > 0:
         time.sleep(1)
         print('.', end="")
+        retry -= 1
 
-    print('connected')
+    return sta_if.isconnected()
 
 def network_config():
     print('Network config:', sta_if.ifconfig())
 
 def try_connect_to_wifi(ssid=SSID, password=PASSWORD):
+    retry = 10
     if sta_if.isconnected():
         print('Already connected')
     else:
-        connect_to_wifi(ssid, password)
+        while not connect_to_wifi(ssid, password) and retry > 0:
+            disable()
+            time.sleep(1)
+            retry -= 1
 
     network_config()
 
 def disable():
-    sta_if.disconnect()
     sta_if.active(False)
     print('disconnected')
 
